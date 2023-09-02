2d90c0f14e12c52f6e7df99591696cc634a0b5b0
mtomardev <120013813+mtomardev@users.noreply.github.com>
2023-09-02 15:41:22 +0530

    Capital

--- src/component/mainComponent/index.js
+++ src/component/mainComponent/index.js
@@ -15,6 +15,30 @@ const MainComponent = () => {
         setUpdateText(newtext)
     }
 
+    // let Capitalize = () =>{
+    //     const words = text.split(" ");
+    //     for(let i=0; i<words.length ; i++){
+    //         words[i] = words[i][0].toUpperCase()+ words[i].substring(1)
+    //         }
+    //     const combineWords = words.join(" ")
+    //     setUpdateText(combineWords)
+    // }
+
+    let Capitalize = () => {
+        const textArray = text.split(" ");
+        const stopWords = ["is", "are", "am", "my"]; // Define the words to keep in lowercase
+    
+        for (let i = 0; i < textArray.length; i++) {
+            const word = textArray[i].toLowerCase(); // Convert word to lowercase
+            if (i === 0 || !stopWords.includes(word)) {
+                textArray[i] = word.charAt(0).toUpperCase() + word.slice(1);
+            }
+        }
+    
+        const capitalizedText = textArray.join(" ");
+        setUpdateText(capitalizedText);
+    }
+
     let Reset = () => {
         setText("")
         setUpdateText("")
@@ -31,6 +55,7 @@ const MainComponent = () => {
            <div className="btn-flex">
             <p onClick={UpperCase}>UpperCase</p>
             <p onClick={Lowercase}>LowerCase</p>
+            <p onClick={Capitalize}>Capitalize</p>
             <p onClick={Reset}>Reset</p>
            </div>
 
